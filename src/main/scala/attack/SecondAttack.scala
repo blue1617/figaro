3bcc0d38fcb40e23656de4ad961ed1bd2549dc8b
Alina <alina_preda200@yahoo.com>
2019-03-04 11:43:17 +0200

    adding second attack

--- src/main/scala/attack/SecondAttack.scala
+++ src/main/scala/attack/SecondAttack.scala
@@ -1,45 +1,90 @@
 package attack
 
+import attack.AverageProgram.{Age, AverageAge, Name}
+import attack.FirstAttack2._
 import com.cra.figaro.algorithm.sampling.Importance
-import com.cra.figaro.language._
-import com.cra.figaro.library.atomic.continuous.Normal
-import com.cra.figaro.library.atomic.discrete.{Binomial, Uniform}
-import com.cra.figaro.library.collection.{FixedSizeArrayElement, VariableSizeArray}
+import com.cra.figaro.language.{Constant, Element}
+import com.cra.figaro.library.atomic.discrete.Uniform
+import com.cra.figaro.library.collection.{FixedSizeArray, FixedSizeArrayElement, VariableSizeArray}
 
+/**
+  * Created by apreda on 27.02.2019.
+  */
 object SecondAttack {
 
+  /**
+    * // The attacker has seen that the average age was a bit above 20,
+    * // but does not know precisely. Rather than specifying hard evidence that
+    * // average_age is a certain value, 22, for example, one can specify soft
+    * // evidence that
+    * // average_age is most likely to be between 20.25 and 23. The age range
+    * // of 20.25 to 23 is 1000 times more likely than any other age.
+    * //Constraint can be seen as either a soft evidence, or a way of providing
+    * // additional connections between elements in a model. A constraint is
+    * // added when one
+    * // has
+    * // some evidence
+    * // about an
+    * // element, but is not quite sure about it.The probability of the average
+    * a ge to be in the given range is 1000 more likely than outside it?
+    *
+    * @param a average age
+    * @return the average age with the intented constraint
+    */
+  def averageAgeConstraint(a: AverageAge): Double = {
+    if (a == 16|| a == 17)
+      1000.0
+    else
+      0.0
+  }
+
+  def ageAttack1(priorKnowledge: FixedSizeArrayElement[(Name, Age)]): Element[Age] = {
+    for { // Element[A]
+      container <- priorKnowledge.element
+      idx <- container findIndex {
+        _._1 == "Tom"
+      } map {
+        _ getOrElse -1
+      }
+      tom <- container get idx
+      age = tom map (_._2)
+    } yield age getOrElse -1
+  }
 
 
-/**   alpha ( John, 17 :: Tom,15)
-    returns 16
-    an attacker that knows the input list.
-    a1 = Constant ( List((John,17),(Tom,15) )  )
-    alpha_p (a1)
-    returns Constant (16)*/
-
-  //scala test if I call on this on line 99 is 0, under 42 should 1 should
-  // include model, inference, constant attacker, age of Alice, everything
-  // that is in main , add more composition as tests
-
-  //a third attacker model
+  def generateFirstAttacker(dict: Seq[String], ages: Seq[Age]): Element[(Name, Age)] = {
+    for {name <- Uniform(dict: _*)
+         a <- Uniform(ages: _*)} yield (name, a)
+  }
 
 
   def main(args: Array[String]): Unit = {
 
+    val dict: Seq[Name] = List("John", "Tom")
+    val ages: Seq[Age] = List(16, 17)
+    //first attacker
+    val priorFirstAttackerArray: FixedSizeArray[(Name, Age)] = new FixedSizeArray[(Name, Age)](2, i =>
+      generateFirstAttacker(dict, ages))
+    val priorFirstAttacker: FixedSizeArrayElement[(Name, Age)] = new FixedSizeArrayElement(Constant(priorFirstAttackerArray))
 
+    // This is what we know about average age before any observation
+    val average_age: Element[AverageAge] = AverageProgram.alpha_p(priorFirstAttacker)
+    // The attacker knows that Tom should be in the list
+    val seenTom = priorFirstAttacker exists { case (s, a) => s == "Tom" }
+    seenTom.observe(true)
+
+    average_age.addConstraint(a => averageAgeConstraint(a))
+
+    val ageOfTomElement: Element[Age] = ageAttack1(priorFirstAttacker)
+
+    // How sure is the attacker that Tom is 16?
+    val attack1: Double = Importance.probability(ageOfTomElement, (a: Double) => a == 16)//this prints  0.5024885526577609
+    println("attack 1 probability " + attack1)
+
+    val attack2: Double = Importance.probability(average_age, (a: Double) => a == 16)//this prints 0.50271465915945
+
+    println("attack 2 probability " + attack2)
 
   }
 
-
-
-  /*
-
-    a2 = Select (  List((John,17),(Tom,15) -> 0.5; List((John,27),(Tom,25 ) -> 0.5 ) )
-    alpa_p (a2)
-    alpha_p (a2).observe (16)
-    a2.probability ( l => l.size == 2)
-    a2.probability (l => l.exists ( x => x == (John,15))
-  */
 }
-
-
