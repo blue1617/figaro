3b95145a5ac6c811c3032da556d0632eca7b5371
Alina <alina_preda200@yahoo.com>
2019-10-14 10:17:27 +0300

    changing fifth attacker

--- src/test/scala/attack/FifthAttackerTest.scala
+++ src/test/scala/attack/FifthAttackerTest.scala
@@ -18,21 +18,17 @@ class FifthAttackerTest extends FlatSpec {
 
 
     // How sure is the attacker that Alice is 15?
-    val attack1: Double = attacker.getAttackProbability((a: Double) => a == 16)
-    assert(attack1 < 0.5) //we have a uniform distribution on age ranging from 15 to 50
-    println("attack1 " + attack1)
+    val attack1: Double = attacker.getAttackProbability((a: Double) => a == 18)
+    assert(attack1 < 0.5) //Alice's age has a normal distribution  Normal(18, 1)
+    println("Alice is age 18 " + attack1)
+
+    val attack2: Double = attacker.getAttackProbability((a: Double) => a < 18)
+    assert(attack2 < 0.6)
+    println("Alice is underage " + attack2)//this prints more than 0.5
 
 
     //    val attack1Belief: Double = BeliefPropagation.probability(ageOfAliceElement, (a: Double) => a == 15)
     //    assert(attack1Belief < 0.5)//we have a uniform distribution on age ranging from 15 to 50
     //    println("attack1Belief " + attack1Belief)
-
-    val attack1Prime: Double = attacker.getAttackProbability((a: Double) => a == 50)
-    assert(attack1Prime < 0.3)
-    //    println("attack1Prime " + attack1Prime)
-
-    val attack2: Double = attacker.getAttackProbability((a: Double) => a < 18) //this prints
-    // 0.06813750641354209
-    assert(attack2 < 0.5)
   }
 }
