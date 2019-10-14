5f8c26b3836327ef8c83611c26b48e33343776db
Alina <alina_preda200@yahoo.com>
2019-10-14 10:33:42 +0300

    changing Andrzej's attacker

--- src/main/scala/attack/SlideAttacker.scala
+++ src/main/scala/attack/SlideAttacker.scala
@@ -7,27 +7,33 @@ import com.cra.figaro.library.atomic.discrete.{Binomial, Uniform}
 import com.cra.figaro.library.collection.{FixedSizeArrayElement, VariableSizeArray}
 
 /**
+  * There are Binomial(5, 0.3) people in the database. Alice's age is Normal (18, 2) and the other people in the are
+  * Normal (42, 2).
+  * The constraint on the age is between 20.25 and 23.
   * Created by apreda on 28.02.2019.
   */
 class SlideAttacker extends Attacker {
 
-  val names: Seq[Name] = List("John", "Alice", "Joe", "Bob", "Tom")
+  val names: Seq[Name] = List("John", "Joe", "Bob", "Tom")
   val ages: Seq[Age] = List() //not used
-  override val populationSize: Int = 3
+  override val populationSize: Int = 5
   override val averageConstraint: Double => Double = a => AverageProgram.averageAgeConstraint((a >= 20.25) && (a <
     23.00))
+  var distributionStandardDeviation: Double = 2
 
-  //todo: plot variations of the attackers by changing the standard deviation on the OX AND OY HAVE THE PROBABILITY
+  //todo: plot variations of the attackers by changing the standard deviation on the OX AND on OY HAVE THE
+  // PROBABILITY of the attack
   val prior: FixedSizeArrayElement[(Name, Age)] = VariableSizeArray(
     numItems = Binomial(populationSize, 0.3) map {
       _ + 1
     }, //todo: we currently have a prior on the Alice's age, so let's add a likelihood of average
     generator = i => for {
       n <- if (i == 0) Constant("Alice") else Uniform(names: _*)
-      a <- if (i == 0) Normal(16, 1) else Normal(20.0, 20.0)}
+      a <- if (i == 0) Normal(18, Math.pow(distributionStandardDeviation, 2)) else Normal(42.0, Math.pow
+      (distributionStandardDeviation, 2))}
       yield (n, a)
     // Normal(42.0, 500.0),Normal(20.0, 20.0) 20 is the mean and the variance, which is the square of the standard
-    // deviation is 20 (around 4)
+    // deviation, which is around 4
     //todo: change the inference algorithm to variable elimination
     //todo: add another continuous distribution
     //my scenarios plot the prior distribution and posterior distribution to show that the attacker has learned a
--- src/test/scala/attack/SlideAttackerTest.scala
+++ src/test/scala/attack/SlideAttackerTest.scala
@@ -17,22 +17,14 @@ class SlideAttackerTest extends FlatSpec {
 
 
     val ageOfAliceElement: Element[Age] = new SlideAttacker().getAttackElement
-    ageOfAliceElement.setCondition((a: Double) => a < 18.0)
+//    ageOfAliceElement.setCondition((a: Double) => a < 18.0) todo: adding this condition lifts the inference to 9.99
     // How sure is the attacker that Alice is underage?
-    val attackVariableElimination: Double = Importance.probability(ageOfAliceElement, (a: Double) => a < 18.0)
+    val attack: Double = Importance.probability(ageOfAliceElement, (a: Double) => a < 18.0)
 
-    println("slide attack probability " + attackVariableElimination) //this prints out NaN with Normal(42.0, 20.0) since the constraint
-//     says that the range is between 20 and 23
-    assert(attackVariableElimination > 0.99)
-
-    //importance sampling
-    val importanceSampling = Importance(ageOfAliceElement)
-    importanceSampling.start()
-    Thread.sleep(1000)
-    val attack2ImportanceSampling: Double = importanceSampling.probability(ageOfAliceElement, (a: Double) => a < 18)
-    assert(attack2ImportanceSampling > 0.99)//this prints :0.9999999999999949
-    println("attack2ImportanceSampling " + attack2ImportanceSampling)
-    importanceSampling.kill()
+    println("slide attack probability " + attack) //this prints out NaN with Normal(42.0, 20.0) since the constraint
+//     says that the range is between 20 and 23 and the Normal distribution says that the average of the age is 42
+    //     and the standard deviation is sqrt(20), around 4, so the range is between 38 and 46
+    assert(attack < 0.1)// this prints 0.024948317749016163
   }
 
 }
